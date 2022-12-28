218f80f500d1487a02a38111111dc949bd9f1ba1
Noa <noasalgadodev@gmail.com>
2022-12-28 09:38:12 +0100

    Upgrade to functional component using hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,52 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import CardList from '../components/CardList';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: '',
-    };
-  }
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) => setRobots(users));
+  }, []);
 
-  //Event handler. Using arrow functions this refers to App
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
   };
 
-  render() {
-    const filteredRobots = this.state.robots.filter((robot) =>
-      robot.name
-        .toLocaleLowerCase()
-        .includes(this.state.searchField.toLocaleLowerCase())
-    );
+  const filteredRobots = robots.filter((robot) =>
+    robot.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase())
+  );
 
-    if (this.state.robots.length === 0) {
-      return <h1>LOADING....</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="navy f-5">RobotLand</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundary>
-          </Scroll>
-        </div>
-      );
-    }
+  if (robots.length === 0) {
+    return <h1>LOADING....</h1>;
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="navy f-5">RobotLand</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
   }
-}
+};
 
 export default App;
