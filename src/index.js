cf26a3c261de3fb9e058783816dce99088f922ef
Noa <noasalgadodev@gmail.com>
2023-01-08 22:38:36 +0100

    Add redux for state managment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { requestRobots, searchRobots } from './reducers';
+
 import './index.css';
 import 'tachyons';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 
+const logger = createLogger();
+
+const rootReducers = combineReducers({ requestRobots, searchRobots });
+
+const store = createStore(
+  rootReducers,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
